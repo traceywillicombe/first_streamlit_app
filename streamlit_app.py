23e4189636e8fb34f8376b25b646231c8857e112
Tracey Willicombe <109740927+traceywillicombe@users.noreply.github.com>
2022-07-22 17:16:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My parents New Healthy Diner')
 
-streamlit.header('Breakfast')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-boiled free-range eggs')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
