cc0e17d63240399313b46042d7cb0621dcac68a3
Tracey Willicombe <109740927+traceywillicombe@users.noreply.github.com>
2022-07-28 15:26:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ try:
     back_from_function= get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
     
-#except URLError as e:
-#  streamlit.error()
+except URLError as e:
+    streamlit.error()
 
 streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
